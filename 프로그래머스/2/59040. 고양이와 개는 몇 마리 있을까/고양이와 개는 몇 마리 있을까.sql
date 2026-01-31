4b0f73d9bfd2884e0ce5b74029f1e4e67f6cae5d
SEUNG JIN BAEK <jean0455@naver.com>
2026-01-31 16:24:48 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,4 +1,5 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_TYPE, count(*) as count from ANIMAL_INS 
-group by ANIMAL_TYPE
-order by ANIMAL_TYPE; 
+where animal_type in ('Cat', 'Dog')
+group by ANIMAL_TYPE 
+order by ANIMAL_TYPE;
