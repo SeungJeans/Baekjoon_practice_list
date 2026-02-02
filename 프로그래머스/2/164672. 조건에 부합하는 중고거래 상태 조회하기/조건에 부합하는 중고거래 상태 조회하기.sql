2b89acee99026cab7f4f7fd5a9e74ba853a193c6
SEUNG JIN BAEK <jean0455@naver.com>
2026-02-02 16:59:23 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+case 
+    when STATUS = "SALE" then "판매중" 
+    when STATUS = "RESERVED" then "예약중"
+    when STATUS = "DONE" then "거래완료"
+end
+as STATUS
+from USED_GOODS_BOARD 
+WHERE CREATED_DATE LIKE '2022-10-05%' 
+order by BOARD_ID desc;
